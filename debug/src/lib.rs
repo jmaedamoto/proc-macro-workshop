6df5546118f3151e579ebad71c8b19210c30846b
枝元 勝悟 <jma8000@os.met.kishou.go.jp>
2021-04-02 08:46:46 +0900

    debug - 1 complete

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    TokenStream::new()
 }
--- debug/tests/progress.rs
+++ debug/tests/progress.rs
@@ -1,7 +1,7 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    //t.pass("tests/01-parse.rs");
+    t.pass("tests/01-parse.rs");
     //t.pass("tests/02-impl-debug.rs");
     //t.pass("tests/03-custom-format.rs");
     //t.pass("tests/04-type-parameter.rs");
