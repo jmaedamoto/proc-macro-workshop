3ff6a3dd73c3759dffa633ecc3ad6b6be184342f
枝元 勝悟 <jma8000@os.met.kishou.go.jp>
2021-04-06 11:14:57 +0900

    seq - 1 complete

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,31 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, Ident, LitInt, Token, braced};
+use syn::parse::{Parse};
+
+struct Item {
+    ident: Ident,
+}
+
+impl Parse for Item {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let ident: Ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let _ : LitInt = input.parse()?;
+        input.parse::<Token![..]>()?;
+        let _ : LitInt = input.parse()?;
+        let content;
+        let _ = braced!(content in input);
+        Ok(Item{
+            ident
+        })
+    }
+}
+
+
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as Item);
 
-    unimplemented!()
+    TokenStream::new()
 }
