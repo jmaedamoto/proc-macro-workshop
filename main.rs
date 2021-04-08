8094e177ae200ae5656a5f9b41a39ff9a13aeaa5
枝元 勝悟 <jma8000@os.met.kishou.go.jp>
2021-04-08 10:52:42 +0900

    soreted - 4 complete

--- main.rs
+++ main.rs
@@ -5,19 +5,22 @@
 //
 // To run the code:
 //     $ cargo run
-
+#![allow(unused_imports)]
 use sorted::sorted;
 
-#[sorted]
-pub struct Error {
-    kind: ErrorKind,
-    message: String,
-}
+use std::env::VarError;
+use std::error::Error as StdError;
+use std::fmt;
+use std::io;
+use std::str::Utf8Error;
 
-enum ErrorKind {
-    Io,
-    Syntax,
-    Eof,
+#[sorted]
+pub enum Error {
+    Fmt(fmt::Error),
+    Io(io::Error),
+    Utf8(Utf8Error),
+    Var(VarError),
+    Dyn(Box<dyn StdError>),
 }
 
 fn main() {}
--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,15 +1,31 @@
 use proc_macro::TokenStream;
 use proc_macro2::{TokenStream as TokenStream2, Span};
-use syn::{Item, parse_macro_input};
+use syn::{Item, ItemEnum, parse_macro_input};
+
+fn mk_error(span: Span, message: &str) -> TokenStream{
+    TokenStream::from(syn::Error::new(span,message).into_compile_error())
+}
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = TokenStream2::from(args);
     let input = parse_macro_input!(input as Item);
-    if let Item::Enum(_) = input{
-
+    if let Item::Enum(ItemEnum{ref variants,..}) = input{
+        let variants = variants.clone().into_pairs().map(|pair|{
+            pair.value().ident.clone()
+        }).collect::<Vec<_>>();
+        for i in 1..variants.len(){
+            let ident = variants.get(i).unwrap();
+            for j in 0..(i - 1){
+                let prev_ident = variants.get(j).unwrap();
+                if prev_ident.to_string() > ident.to_string(){
+                    let message = format!("{} should sort before {}",ident.to_string(),prev_ident.to_string());
+                    return mk_error(ident.span(), &message);
+                }
+            }
+        }
     }else{
-        return TokenStream::from( syn::Error::new(Span::call_site(),"expected enum or match expression").into_compile_error());
+        return mk_error(Span::call_site(),"expected enum or match expression");
     }
 
     TokenStream::new()
