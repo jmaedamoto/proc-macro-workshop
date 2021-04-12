8fa22b7b09bf5c42231c97b8b039669fc8e99f85
枝元 勝悟 <jma8000@os.met.kishou.go.jp>
2021-04-12 17:23:09 +0900

    作業途中

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,27 +1,37 @@
 use proc_macro::TokenStream;
 use proc_macro2::{TokenStream as TokenStream2, Span};
-use syn::{Item, parse_macro_input};
-use quote::quote;
+use syn::{Item, Ident,  parse_macro_input};
+use quote::{quote, format_ident};
 
 fn mk_error(span: Span, message: &str) -> TokenStream2{
     syn::Error::new(span,message).into_compile_error()
 }
 
+struct BitField{
+    name: Ident,
+    bit_type: Ident
+}
+
 #[proc_macro_attribute]
 pub fn bitfield(_: TokenStream, input: TokenStream) -> TokenStream {
+
     let input = parse_macro_input!(input as Item);
     let visibility: syn::Visibility;
     let ident: syn::Ident;
-    let mut bit_types = Vec::new();
+    let mut bit_fields = Vec::new();
 
     match input {
         syn::Item::Struct(item_struct) => {
             visibility = item_struct.vis;
             ident = item_struct.ident;
             item_struct.fields.iter().for_each(|field|{
-                if let syn::Type::Path(syn::TypePath{ref path,..}) = field.ty{
-                    let bit = path.get_ident().unwrap();
-                    bit_types.push(quote!{ + <#bit as Specifier>::BITS});
+                if let syn::Type::Path(syn::TypePath{ref path,..}) = &field.ty{
+                    let bit_type = path.get_ident().unwrap().clone();
+                    let name = field.ident.as_ref().unwrap().clone();
+                    bit_fields.push(BitField{
+                        name,
+                        bit_type
+                    });
                 }
             })
         }
@@ -30,12 +40,46 @@ pub fn bitfield(_: TokenStream, input: TokenStream) -> TokenStream {
         }
     }
 
-    let size = quote!{(0 #(#bit_types)*) / 8};
+    let bit_types = bit_fields.iter().map(|bit_field|{
+        let bit_type = &bit_field.bit_type;
+        quote!{<#bit_type as Specifier>::BITS}
+    });
+
+    let size = quote!{(0 #(+ #bit_types)*) / 8};
+
+    //let mut setters = Vec::new();
+    //let mut getters = Vec::new();
+
+    bit_fields.iter().enumerate().for_each(|(i,bit_field)|{
+        
+        let setter = quote!{
+            fn set_c(&mut self, c: u64){
+                let byte = (<B1 as Specifier>::BITS + <B3 as Specifier>::BITS) / 8;
+                let bit = <B1 as Specifier>::BITS + <B3 as Specifier>::BITS;
+        
+                let mut mask:[u8;4] = [255,255,255,255];
+                mask[byte] = mask[byte].checked_shl(bit as u32).unwrap() as u8;
+        
+                let mut value:[u8;4] = [0,0,0,0];
+                value[byte] = c.checked_shl(bit as u32).unwrap() as u8;
+        
+                for i in 0..self.data.len(){
+                    self.data[i] = self.data[i] & mask[i] | value[i];
+                }
+            }
+        };
+    });
 
     (quote!{
         #[repr(C)]
         #visibility struct #ident {
             data: [u8; #size],
+        }
+
+        impl #ident {
+            fn new() -> #ident{
+                #ident{data: [0u8; #size]}
+            }
         }
     }).into()
 }
--- main.rs
+++ main.rs
@@ -16,6 +16,59 @@ pub struct MyFourBytes {
     d: B24,
 }
 
+// #[repr(C)]
+// pub struct MyFourBytes {
+//     data: [u8; (0
+//         + <B1 as Specifier>::BITS
+//         + <B3 as Specifier>::BITS
+//         + <B4 as Specifier>::BITS
+//         + <B24 as Specifier>::BITS)
+//         / 8],
+// }
+// impl MyFourBytes {
+//     fn new() -> MyFourBytes {
+//         MyFourBytes {
+//             data: [0u8; (0
+//                 + <B1 as Specifier>::BITS
+//                 + <B3 as Specifier>::BITS
+//                 + <B4 as Specifier>::BITS
+//                 + <B24 as Specifier>::BITS)
+//                 / 8],
+//         }
+//     }
+
+//     fn set_c(&mut self,c: u64){
+//         let byte = (<B1 as Specifier>::BITS + <B3 as Specifier>::BITS) / 8;
+//         let bit = <B1 as Specifier>::BITS + <B3 as Specifier>::BITS;
+
+//         let mut mask:[u8;4] = [255,255,255,255];
+//         mask[byte] = mask[byte].checked_shl(bit as u32).unwrap() as u8;
+
+//         let mut value:[u8;4] = [0,0,0,0];
+//         value[byte] = c.checked_shl(bit as u32).unwrap() as u8;
+
+//         for i in 0..self.data.len(){
+//             self.data[i] = self.data[i] & mask[i] | value[i];
+//         }
+//     }
+
+//     fn get_c(&mut self) -> u64{
+//         let byte = (<B1 as Specifier>::BITS + <B3 as Specifier>::BITS) / 8;
+//         let bit = <B1 as Specifier>::BITS + <B3 as Specifier>::BITS;
+//         (self.data[byte].checked_shr(bit as u32).unwrap()) as u64
+//     }
+// }
+
 fn main() {
-    assert_eq!(std::mem::size_of::<MyFourBytes>(), 4);
+    let mut bitfield = MyFourBytes::new();
+    //assert_eq!(0, bitfield.get_a());
+    //assert_eq!(0, bitfield.get_b());
+    //assert_eq!(0, bitfield.get_c());
+    //assert_eq!(0, bitfield.get_d());
+
+    //bitfield.set_c(14);
+    //assert_eq!(0, bitfield.get_a());
+    //assert_eq!(0, bitfield.get_b());
+    //assert_eq!(14, bitfield.get_c());
+    //assert_eq!(0, bitfield.get_d());
 }
